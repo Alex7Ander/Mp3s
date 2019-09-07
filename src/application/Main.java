8896c241c27772cec38ff44a5682c78556fe52ff
Alex7Ander <alex.ander.1991@mail.ru>
2019-09-07 20:23:43 +0300

    Main window start creating

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,27 @@
 package application;
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) { 
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainForm.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Mp3s");
+			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		}
+		catch(IOException ioExp) {
+			
 		}
 	}
 	
