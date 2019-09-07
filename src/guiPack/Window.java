84bb161a9a6b0fcc0c65a270e677bdda7ddc7bb0
Alex7Ander <alex.ander.1991@mail.ru>
2019-09-07 18:44:17 +0300

    Initial Commit

---
+++ src/guiPack/Window.java
@@ -0,0 +1,46 @@
+package guiPack;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public abstract class Window {
+
+	protected Parent root;
+	protected Stage stage;
+	protected Scene scene;
+	protected String title;
+	protected String fxmlFile;
+	protected FXMLLoader loader;
+	protected WindowController controller;
+	
+	public Window(String Title, String FXMLFile) {
+		this.title = Title;
+		this.fxmlFile = FXMLFile;
+		try {
+			this.loader = new FXMLLoader(getClass().getResource(fxmlFile));
+			this.root = loader.load();
+			this.scene = new Scene(root);
+			this.stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle(title);
+		}
+		catch(IOException ioExp) {
+			
+		}
+		
+	}
+	
+	public void show() {
+		stage.show();
+	}
+	
+	public void close() {
+		stage.close();
+	}
+	
+	public abstract WindowController getControllerClass();
+}
---
+++ src/guiPack/WindowController.java
@@ -0,0 +1,5 @@
+package guiPack;
+
+public abstract class WindowController {
+
+}
